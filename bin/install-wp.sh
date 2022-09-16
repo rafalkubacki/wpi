82763cb7e53dbd0bed0a1dba34398bc5e85702a4
Rafał Kubacki <rafalkubacki7@gmail.com>
2022-09-16 16:40:54 +0200

    wordpress setup update

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -9,9 +9,32 @@ wp core install \
   --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
   --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 
-# Update permalink structure.
+wp language core install "${WORDPRESS_LANGUAGE}"
+wp site switch-language "${WORDPRESS_LANGUAGE}"
+
+# Update WordPress options.
 wp option update \
   permalink_structure "${WORDPRESS_PERMALINK_STRUCTURE}" \
+  --skip-themes \
+  --skip-plugins
+
+wp option update \
+  blogdescription "${WORDPRESS_TAGLINE}" \
+  --skip-themes \
+  --skip-plugins
+
+wp option update \
+  timezone_string "${WORDPRESS_TIMEZONE}" \
+  --skip-themes \
+  --skip-plugins
+
+wp option update \
+  date_format "${WORDPRESS_DATEFORMAT}" \
+  --skip-themes \
+  --skip-plugins
+
+wp option update \
+  time_format "${WORDPRESS_HOURFORMAT}" \
   --skip-themes \
   --skip-plugins
 
@@ -32,6 +55,20 @@ then
 fi
 
 wp plugin install ${WORDPRESS_PLUGINS_TO_INSTALL} --activate
+
+# Clear installation
+wp post delete $(wp post list --post_type='page' --format=ids) --force --defer-term-counting
+wp post delete $(wp post list --post_type='post' --format=ids) --force --defer-term-counting
+wp post delete $(wp post list --post_type='wpcf7' --format=ids) --force --defer-term-counting
+wp comment delete $(wp comment list --format=ids) --force
+
+# Setup basic data
+HOME_ID=$(wp post create --post_type=page --post_title='Home' --post_status=publish --porcelain)
+wp menu create "Nav"
+wp menu item add-post nav ${HOME_ID}
+
+wp option update show_on_front page
+wp option update page_on_front ${HOME_ID}
 
 echo -e "\nREPORT\n"
 
